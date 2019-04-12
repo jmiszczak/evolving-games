dadd7d3e14785bed99e106aede194211e5e0d1da
Jarek Miszczak <miszczak@iitis.pl>
2019-04-12 20:25:19 +0200

    Fixed imitation process

--- games/pd.py
+++ games/pd.py
@@ -1,8 +1,19 @@
 import networkx as nx
-import numpy as np
 import random as rnd
-import matplotlib.pyplot as plt
 
+# --------------------------------------------------------------------------
+# Implementation of functionality required to run Prisoner's Dilemma game on
+# a graph. Provides methods for initializing strategies and payoffs,
+# for evaluating payoffs, and for updating strategies.
+#
+# A single run of a gam on a graph is executed using pd_play.
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Initialization functions.
+# ---------------------------------------------------------------------------
+
+#
 # initial strategies distributed with equal probability
 def pd_init_strategy(g, strats = ['C', 'D']):
     for n in g.nodes:
@@ -17,6 +28,11 @@ def pd_init_payoff(g, init_v = 0):
         g.nodes[n]['v'] = init_v
 
     return g
+
+
+# ---------------------------------------------------------------------------
+# Evaluation of the payoff function.
+# ---------------------------------------------------------------------------
 
 # payoff function for two payers n1 and n2
 def pd_payoff(g, n1, n2, r, t):
@@ -35,6 +51,7 @@ def pd_payoff(g, n1, n2, r, t):
             pf = [0, t]
     return pf
 
+
 # accumulated payoff for player n playing with all neighbors
 def pd_local_payoff(g, n, r, t):
     lpf = 0
@@ -43,6 +60,7 @@ def pd_local_payoff(g, n, r, t):
 
     return lpf
 
+
 # update payoff for all players by playing with all neighbors
 def pd_update_payoff(g, r, t):
     for n in g.nodes:
@@ -50,7 +68,11 @@ def pd_update_payoff(g, r, t):
 
     return g
 
-# imitation process
+
+# ---------------------------------------------------------------------------
+# Imitation process based on the accumulated payoff.
+# ---------------------------------------------------------------------------
+
 def pd_update_strategy(g, r, t):
     for n in g.nodes:
         # select one of the neighbors
@@ -67,17 +89,13 @@ def pd_update_strategy(g, r, t):
         pim = pd/(t*max(g.degree(rn), g.degree(n))) if pd>0 else 0
        
         # update the strategy
-        #if pim>0 and sr =='C':
-        #    print("[INFO] pim=", pim)
-        #    print(sn, '->',sr, [g.nodes[nn]['s'] for nn in g.neighbors(n)])
-        ns = rnd.choices([g.nodes[rn]['s'], g.nodes[n]['s']], [pim, 1-pim])
-        g.nodes[n]['s'] = g.nodes[rn]['s']
-        
-        #if pim > rnd.uniform(0,1):
-        #    g.nodes[n]['s'] = g.nodes[rn]['s']
+        g.nodes[n]['s'] = rnd.choices([sr, sn], [pim, 1-pim])[0]
 
     return g
 
+# ---------------------------------------------------------------------------
+# Execution of the game on the given graph.
+# ---------------------------------------------------------------------------
 
 def pd_play(g = None, r = 1, t = 2, g0 = None):
     """
