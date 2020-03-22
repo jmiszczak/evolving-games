bc8dc856ff5d7946523c5247c01bd9b23ddea698
Jarek Miszczak <miszczak@iitis.pl>
2020-03-22 13:23:20 +0100

    Money model from Mesa tutorial

---
+++ money/MoneyModel.py
@@ -0,0 +1,31 @@
+import mesa as ms
+import mesa.time as mt
+import numpy.random as rnd
+
+class MoneyAgent(ms.Agent):
+    """An agent with initial amount of money"""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.wealth = 1
+
+    def step(self):
+        """Execute one step"""
+        if self.wealth == 0 :
+            pass
+        else :
+            other = self.random.choice(self.model.schedule.agents)
+            other.wealth += 1
+            self.wealth -= 1
+
+class MoneyModel(ms.Model):
+    def __init__(self, N):
+        self.num_agents = N
+        self.schedule = mt.RandomActivation(self)
+        # create and add agents
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        """Execute one step for all agents"""
+        self.schedule.step()
