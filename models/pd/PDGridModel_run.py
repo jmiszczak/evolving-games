4a6188f68c78c140fe387050ae2f13a7d9082f3f
Jarek Miszczak <miszczak@iitis.pl>
2020-05-11 20:37:54 +0200

    Added simple strategy evolution

--- models/pd/PDGridModel_run.py
+++ models/pd/PDGridModel_run.py
@@ -1,4 +1,8 @@
 #%%
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
 
 from PDGridModel import PDGridModel, defectors_rate
 
@@ -11,10 +15,12 @@ mpl.rc('font', size = 14)
 
 from IPython.core.display import display
 
+import numpy as np
+
 #%%
 
 # store data from 100 num_runs
-num_runs = 50
+num_runs = 20
 
 # each run has num_steps steps
 num_steps = 10
@@ -26,20 +32,21 @@ width, height = (20,20)
 temptation = 1
 
 # number of active agents
-num_agents = 2*width*height
+num_agents = int(width*height/2)
 
 # data from all simulations
 strategy_data = []
+wealth_data = []
 
 
 # In[]
 fixed_params = {
         "width": width,
         "height": height,
-        "temptation": temptation
+        "N": num_agents        
         }
 
-variable_params = { "N" : range(10, 101, 10) }
+variable_params = { "temptation" : np.arange(0,4,0.5) }
 
 batch_run = mb.BatchRunner(
         PDGridModel,
@@ -62,7 +69,9 @@ print(run_data)
 
 fig = mpl.figure.Figure(figsize=(4,3))
 axs = fig.add_subplot(111)
-axs.scatter(run_data.N, run_data.Defectors_rate)
+axs.set_xlabel("Temptation")
+axs.set_ylabel("Fraction of defectors")
+axs.scatter(run_data.temptation, run_data.Defectors_rate)
 
 display(fig)
 
