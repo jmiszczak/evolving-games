df1b0f41362b52cab8e31023724ea3b2d29bbdbb
Jarek Miszczak <miszczak@iitis.pl>
2020-11-15 18:18:39 +0100

    Minor in the data management part

--- models/parrondo/ParrondoGridModel_batch.py
+++ models/parrondo/ParrondoGridModel_batch.py
@@ -96,18 +96,20 @@ run_data =  batch_run.get_model_vars_dataframe()
 run_data.rename(columns = {'default_policy': 'N', 'num_agents': 'default_policy'}, inplace =  True)
 run_data.to_csv("data/"+exp_desc+".zip", index=False, compression=dict(method='zip', archive_name='data.csv'))
 
-#%%
-# run_data = pd.read_csv(os.path.dirname(__file__) + "/data/"+exp_desc+".zip")
+#%% plot data
+run_data = pd.read_csv(script_path+ "/data/"+exp_desc+".zip")
+gini_data = np.loadtxt(script_path+"/data/gini_index_values.dat")
 
 fig = mpl.figure.Figure(figsize=(8,8))
 for i,curr_policy in enumerate(['A', 'B', 'AB', 'uniform']):
 
     axs = fig.add_subplot(221+i)
     plot_desc = 'game sequence: '+curr_policy+', grid=(' + str(grid_width) +','+str(grid_height) +')'
-    axs.grid()
+    axs.grid(alpha=0.5,ls='--')
     axs.scatter(run_data[(run_data.default_policy==curr_policy)].N, run_data[(run_data.default_policy==curr_policy)]['Gini index'], marker='x')
+    axs.plot(gini_data,"k:")
     #axs.set_xlabel('Number of agents')
-    axs.set_xlim((1,100))
+    axs.set_xlim((9,101))
     axs.set_ylim((-0.01,1))
     # axs.set_ylabel('Gini index')
     axs.set_title(plot_desc)
