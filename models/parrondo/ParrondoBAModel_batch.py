8143d0164404ad96e47fe1e374a3ca7279a5ada3
Jarek Miszczak <miszczak@iitis.pl>
2020-11-18 18:37:08 +0100

    Testing biased interpretations

--- models/parrondo/ParrondoBAModel_batch.py
+++ models/parrondo/ParrondoBAModel_batch.py
@@ -44,10 +44,10 @@ import indicators
 #%% simulation parameters for batch execution
 
 # initial capital
-init_wealth = 2
+init_wealth = 10
 
 # bias in the Parronod scheme
-default_eps = 0.002
+default_eps = 0.005
 
 ### graph used in the experiemnt
 # size of the BA network
@@ -71,12 +71,12 @@ fixed_params = {
         "graph_spec": ba_graph_file_path,
         "init_wealth": init_wealth,
         "default_eps": default_eps,
-        "default_boost" : "strongmatthew"
         }
 
 variable_params = { 
         "num_agents" : range(10, 101, 10),
-        "default_policy" : ['A', 'B', 'AB', 'uniform']
+        "default_policy" : ['A', 'B', 'AB', 'uniform'],
+        "default_boost" : ["matthew", "antimatthew", "strongmatthew" ,"strongantimatthew"]
         }
          
 batch_run = mb.BatchRunnerMP(
@@ -84,41 +84,57 @@ batch_run = mb.BatchRunnerMP(
         nr_processes = 8,
         variable_parameters=variable_params,
         fixed_parameters=fixed_params,
-        iterations=5,
-        max_steps=200,
+        iterations=50,
+        max_steps=1000,
         model_reporters={
             "Gini index" : indicators.gini_index
             }
         )
 
-exp_desc = "graphBA("+str(network_size)+','+str(m_param)+")_"+str(batch_run.iterations)+"runs_"+str(batch_run.max_steps)+"steps"
-
+exp_desc = "graphBA("+str(network_size)+','+str(m_param)+")_"+str(batch_run.iterations)+"runs_"+str(batch_run.max_steps)+"steps_" + str(default_eps)
 #%% run the experiment
-print("[INFO] Executing", len(variable_params["num_agents"])*len(variable_params["default_policy"])*batch_run.iterations, "iterations.", flush=True)
+print("[INFO] Executing", len(variable_params["num_agents"])*len(variable_params["default_policy"])*len(variable_params["default_boost"])*batch_run.iterations, "iterations.", flush=True)
 batch_run.run_all()
 
 #%% results form the batch execution
 run_data =  batch_run.get_model_vars_dataframe()
-run_data.rename(columns = {'default_policy': 'N', 'num_agents': 'default_policy'}, inplace =  True)
+# workaround for the Mesa bug
+run_data.columns = ['num_agents', 'default_policy', 'default_boost', 'Run', 'Gini index', 'graph_spec', 'init_wealth', 'default_eps']
+
 run_data.to_csv("data/"+exp_desc+".zip", index=False, compression=dict(method='zip', archive_name='data.csv'))
 
 #%% read data from file
 run_data = pd.read_csv(script_path + "/data/"+exp_desc+".zip")
-#run_data.rename(columns = {'default_policy': 'N', 'N': 'default_policy'}, inplace =  True)
 gini_data = np.loadtxt(script_path+"/data/gini_index_values.dat")
 
-#%% plot data
+# %% plot data
 fig = mpl.figure.Figure(figsize=(8,8))
 for i,curr_policy in enumerate(['A', 'B', 'AB', 'uniform']):
 
     axs = fig.add_subplot(221+i)
-    plot_desc = 'game sequence: '+curr_policy+", graphBA("+str(network_size)+','+str(m_param)+')'
+    plot_desc = 'game: '+curr_policy+", BA("+str(network_size)+','+str(m_param)+')'
     axs.grid(alpha=0.5,ls='--')
-    axs.scatter(run_data[(run_data.default_policy==curr_policy)].N,run_data[(run_data.default_policy==curr_policy)]['Gini index'],marker='x',color='r')
+    
+    axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'matthew')].num_agents, 
+                run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'matthew')]['Gini index'],
+                marker='x',color='r')
+    
+    axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongmatthew')].num_agents, 
+                run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongmatthew')]['Gini index'],
+                marker='x',color='b')
+    
+    axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'antimatthew')].num_agents, 
+                run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'antimatthew')]['Gini index'],
+                marker='o',color='g')
+    
+    axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongantimatthew')].num_agents, 
+                run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongantimatthew')]['Gini index'],
+                marker='+',color='k')
+    
     axs.plot(gini_data,"k:")
     #axs.set_xlabel('Number of agents')
-    axs.set_xlim((9,101))
-    axs.set_ylim((-0.01,1))
+    axs.set_xlim((0,101))
+    axs.set_ylim((-0.01,0.5))
     # axs.set_ylabel('Gini index')
     axs.set_title(plot_desc)
 
