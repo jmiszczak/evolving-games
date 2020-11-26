7afe56c7c609904eb649b8170454910629cc420b
Jarek Miszczak <miszczak@iitis.pl>
2020-11-26 19:24:57 +0100

    Fixed naming, added two new functions

--- models/indicators.py
+++ models/indicators.py
@@ -9,20 +9,25 @@ Created on Thu Aug 27 10:04:57 2020
 import numpy as np
 
 def gini_index(model):
-    agent_wealths = sorted([agent.wealth for agent in model.schedule.agents])
-    N = model.num_agents
-    #return sum([(2*(i+1)-N-1)*x for i,x in enumerate(agent_wealths) ])/(N*sum(agent_wealths))
-    
-    return sum([abs(xi-xj) for xi in agent_wealths for xj in agent_wealths ]) /(2*N*sum(agent_wealths))
+    agents_capital = sorted([agent.capital for agent in model.schedule.agents])
+    return sum([abs(xi-xj) for xi in agents_capital for xj in agents_capital ]) /(2*model.num_agents*sum(agents_capital))
 
 
-def total_wealth(model):
-    return sum([agent.wealth for agent in model.schedule.agents])
+def total_capital(model):
+    return sum([agent.capital for agent in model.schedule.agents])
 
 
-def mean_wealth(model):
-    return np.mean([agent.wealth for agent in model.schedule.agents])
+def mean_capital(model):
+    return np.mean([agent.capital for agent in model.schedule.agents])
 
 
-def median_wealth(model):
-    return np.median([agent.wealth for agent in model.schedule.agents])
+def median_capital(model):
+    return np.median([agent.capital for agent in model.schedule.agents])
+
+
+def min_capital(model):
+    return min([agent.capital for agent in model.schedule.agents])
+
+
+def max_capital(model):
+    return max([agent.capital for agent in model.schedule.agents])
