6f61dfdbd92aca0adbcc0a83478c3426552b184b
Jarek Miszczak <miszczak@iitis.pl>
2020-11-26 23:23:35 +0100

    New script for batch run

--- models/janosik/Janosik_grid2d_batch.py
+++ models/janosik/Janosik_grid2d_batch.py
@@ -42,7 +42,7 @@ import indicators
 #%% simulation parameters for batch execution
 
 # initial capital
-init_capital = 2
+init_capital = 10
 
 # bias in the game
 default_eps = 0.15
@@ -52,17 +52,19 @@ grid_width = 10
 grid_height = 10
 
 # graph used in the experiments
-graph = nx.generators.lattice.grid_2d_graph(grid_width,grid_height,periodic=True)
-
 graph_id = "w"+str(grid_width) + "_h"+str(grid_height)
 graph_file_path = script_path + '/graphs/grid2d/' + graph_id + ".gz"
-nx.readwrite.write_gexf(graph, graph_file_path)
+
+# graph generation and saving - ca be used only onece for the grid
+# graph = nx.generators.lattice.grid_2d_graph(grid_width,grid_height,periodic=True)
+# nx.readwrite.write_gexf(graph, graph_file_path)
 # nx.draw(graph)
 
-eps_vals =  [0.0, 0.025, 0.05, 0.10, 0.125, 0.15, 0.25, 0.3, 0.5]
+# values of the bias used in the experiments
+eps_vals =  [0.0]+list(-1*np.array([0.025, 0.05, 0.10, 0.125, 0.15, 0.25, 0.3, 0.5]))
+# eps_vals =  [-0.5, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.5]
 
 #%% batch execution of the simulations
-#
 
 fixed_params = {
         "graph_spec": graph_file_path,
@@ -70,7 +72,7 @@ fixed_params = {
         }
 
 variable_params = { 
-        "num_agents" : range(20, 121, 20),
+        "num_agents" : range(20, 141, 20),
         "default_boost" : ["matthew", "antimatthew", "strongmatthew" ,"strongantimatthew"],
         "default_eps" : eps_vals 
         }
@@ -80,8 +82,8 @@ batch_run = mb.BatchRunnerMP(
         nr_processes = 8,
         variable_parameters=variable_params,
         fixed_parameters=fixed_params,
-        iterations=5,
-        max_steps=5,
+        iterations=20,
+        max_steps=300,
         model_reporters={
             "Gini index" : indicators.gini_index,
             "Total capital": indicators.total_capital, 
@@ -92,7 +94,9 @@ batch_run = mb.BatchRunnerMP(
             }
         )
 
+# string describing the experiment
 exp_desc = 'janosik_'+"grid_"+str(grid_width)+'x'+str(grid_height)+"_"+str(batch_run.iterations)+"runs_"+str(batch_run.max_steps)+"steps"#"_" + str(default_eps)+"eps"
+
 #%% run the experiment
 print("[INFO] Executing", len(variable_params["num_agents"])*len(variable_params["default_boost"])*len(variable_params["default_eps"])*batch_run.iterations, "iterations.", flush=True)
 batch_run.run_all()
@@ -100,22 +104,19 @@ batch_run.run_all()
 #%% results form the batch execution
 rd =  batch_run.get_model_vars_dataframe()
 # workaround for the Mesa bug
-rd.columns = ['num_agents', 'default_boost', 'default_eps', 'Run', 'Gini index', 'graph_spec', 'init_wealth']
-
+rd.columns = ['num_agents', 'default_boost', 'default_eps', 'Run', 'Gini index', 'Total capital', 'Mean capital', 'Median capital', 'Min capital', 'Max capital', 'graph_spec', 'init_wealth']
 rd.to_csv("data/"+exp_desc+".zip", index=False, compression=dict(method='zip', archive_name='data.csv'))
 
-# %% plot data
-
+# %% plot Gini data
 plot_label = {"matthew" : "Matthew", "antimatthew" : "anti-Matthew", "strongmatthew" : "strong Matthew", "strongantimatthew": "strong anti-Matthew"}
 plot_marker = {"matthew" : "bx", "antimatthew" : "go", "strongmatthew": "r+", "strongantimatthew": "k^"}
 plt_marker_size = {"matthew" : 32}
 gini_min = {}
 gini_max = {}
 poly_app_deg = 2
-x_vals = range(20,121,20)
-x_vals_dense = range(20,121,10)
+x_vals = range(20,141,20)
+x_vals_dense = range(20,141,10)
 gini_data = np.loadtxt(script_path+"/data/gini_index_values-constant.dat")
-
 
 #%% ploting
 fig = mpl.figure.Figure(figsize=(12,10))
@@ -137,26 +138,12 @@ for i,curr_eps in enumerate(eps_vals ):
         
         axs.plot(x_vals, gini_min[b], plot_marker[b])
         axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_min[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
-        
-    # axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'antimatthew')].num_agents, 
-    #             run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'antimatthew')]['Gini index'],
-    #             marker='*',color='k', s=24, label="anti-Matthew")
-    
-    # axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongmatthew')].num_agents, 
-    #             run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongmatthew')]['Gini index'],
-    #             marker='^',color='b', s=38, label="strong pro-Matthew")
-
-    
-    # axs.scatter(run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongantimatthew')].num_agents, 
-    #             run_data[(run_data.default_policy==curr_policy) & (run_data.default_boost == 'strongantimatthew')]['Gini index'],
-    #             marker='x',color='g', s=24, label="strong anti-Matthew")
-    
+   
         # axs.plot(gini_data)
-
         # axs.plot(x_vals_dense, gini_data[x_vals_dense],"k",linewidth=1, markersize=4)
         #axs.set_xlabel('Number of agents')
         axs.set_xlim((2,x_vals[-1]+15))
-        axs.set_ylim((0.05,0.6))
+        axs.set_ylim((0.0,0.8))
         # axs.set_ylabel('Gini index')
         # axs.legend(ncol=1, columnspacing=0, labelspacing=0.5)
         axs.set_title(plot_desc)
@@ -170,7 +157,7 @@ fig.tight_layout()
 fig.savefig("plots/"+ exp_desc +".pdf")
 fig.savefig("plots/png/"+ exp_desc +".png")
 
-
+# %% plot capital data
 
 # #%% ploting
 # fig = mpl.figure.Figure(figsize=(6,3/4*6))
