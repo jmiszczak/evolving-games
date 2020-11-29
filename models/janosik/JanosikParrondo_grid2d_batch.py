e5aad6a2831de96110dd15771b6f4d7ac845feea
Jarek Miszczak <miszczak@iitis.pl>
2020-11-29 14:58:52 +0100

    Added data saving info

--- models/janosik/JanosikParrondo_grid2d_batch.py
+++ models/janosik/JanosikParrondo_grid2d_batch.py
@@ -58,7 +58,7 @@ graph_file_path = script_path + '/graphs/grid2d/' + graph_id + ".gz"
 # nx.draw(graph)
 
 # bias in the Parronod scheme, policy, number of agents
-default_policies = ['A', 'B', 'AB', 'AABB']
+default_policies = ['A', 'B', 'AB', 'AABB', 'uniform']
 default_eps_vals = [0.1, 0.05, 0.01, 0.005]
 agen_nums = range(20,141,20)
 
@@ -82,8 +82,8 @@ batch_run = mb.BatchRunnerMP(
         nr_processes = 8,
         variable_parameters=variable_params,
         fixed_parameters=fixed_params,
-        iterations=2,
-        max_steps=100,
+        iterations=20,
+        max_steps=300,
         model_reporters={
             "Gini index" : indicators.gini_index
             }
@@ -99,80 +99,81 @@ rd =  batch_run.get_model_vars_dataframe()
 # workaround for the Mesa bug
 rd.columns = ['num_agents', 'default_policy', 'default_eps', 'default_boost', 'Run', 'Gini index', 'graph_spec', 'init_wealth']
 rd.to_csv("data/"+exp_desc+".zip", index=False, compression=dict(method='zip', archive_name='data.csv'))
+print("[INFO] Data saved to: " + "data/"+exp_desc+".zip")
 
-# %% plot Gini data
-plot_label = {"matthew" : "Matthew", "antimatthew" : "anti-Matthew", "strongmatthew" : "strong Matthew", "strongantimatthew": "strong anti-Matthew"}
-plot_marker = {"matthew" : "bs", "antimatthew" : "go", "strongmatthew": "rx", "strongantimatthew": "k+"}
-plt_marker_size = {"matthew" : 5, "antimatthew": 5, "strongmatthew": 6, "strongantimatthew": 6}
-gini_min = {}
-gini_max = {}
-poly_app_deg = 2
-x_vals = agen_nums
-x_vals_dense = range(agen_nums.start,agen_nums.stop,agen_nums.step//2)
-gini_data = np.loadtxt(script_path+"/data/gini_index_values-constant.dat")
-
-#%% ploting
-fig = mpl.figure.Figure(figsize=(12,10))
-for i,curr_policy in enumerate(default_policies):
-    for j,curr_eps in enumerate(default_eps_vals):
-        
-        axs = fig.add_subplot(4,4,1+i+4*j)
-        plot_desc = str(curr_policy)+ ", " + r"$\epsilon$="+str(curr_eps)
-        axs.grid(alpha=0.5,ls=':')
-        
-        axs.plot(x_vals_dense,gini_data[x_vals_dense],'k-.')
-        
-        for b in ["matthew", "strongmatthew", "antimatthew", "strongantimatthew" ]:
-            gini_max[b] = [rd[(rd.default_policy==curr_policy) & (rd.default_boost == b) & (rd.default_eps == curr_eps) ][rd.num_agents==r]['Gini index'].max() for r in x_vals]
-            gini_min[b] = [rd[(rd.default_policy==curr_policy) & (rd.default_boost == b) & (rd.default_eps == curr_eps) ][rd.num_agents==r]['Gini index'].min() for r in x_vals]
-       
-        
-            # axs.plot(x_vals, gini_max[b], plot_marker[b], label=plot_label[b])
-            # axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_max[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
-            
-            # axs.plot(x_vals, gini_min[b], plot_marker[b])
-            # axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_min[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
-            
-            
-            axs.plot(x_vals, gini_max[b], plot_marker[b], fillstyle='none', ms=plt_marker_size[b], label=plot_label[b])
-            axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_max[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
-            
-            axs.plot(x_vals, gini_min[b], plot_marker[b],  fillstyle='none', ms=plt_marker_size[b] )
-            axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_min[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
-            
-       
-            # axs.plot(gini_data)
-            # axs.plot(x_vals_dense, gini_data[x_vals_dense],"k",linewidth=1, markersize=4)
-            #axs.set_xlabel('Number of agents')
-            axs.set_xlim((2,x_vals[-1]+15))
-            axs.set_ylim((0.0,0.8))
-            # axs.set_ylabel('Gini index')
-            # axs.legend(ncol=1, columnspacing=0, labelspacing=0.5)
-            # axs.set_title(plot_desc)
-            
-            axs.text(15, 0.60, plot_desc, rasterized=False, usetex=True)
-            axs.set_xticks(x_vals)
-
-            if i in [6,7,8]:
-                axs.set_xlabel('Number of agents')
-            else:
-                axs.set_xticklabels([])           
-                
-            axs.set_yticks(np.arange(0, 1, step=0.2))
-                
-            if i in [0,4,8,12]:
-                axs.set_ylabel('Gini index')
-            else:
-                axs.set_yticklabels([])
-
-handles, labels = axs.get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5,-0.02), ncol=4)
-
-display(fig)
-
-fig.tight_layout()
-fig.savefig("plots/"+ exp_desc +".pdf")
-fig.savefig("plots/png/"+ exp_desc +".png")
+# # %% plot Gini data
+# plot_label = {"matthew" : "Matthew", "antimatthew" : "anti-Matthew", "strongmatthew" : "strong Matthew", "strongantimatthew": "strong anti-Matthew"}
+# plot_marker = {"matthew" : "bs", "antimatthew" : "go", "strongmatthew": "rx", "strongantimatthew": "k+"}
+# plt_marker_size = {"matthew" : 5, "antimatthew": 5, "strongmatthew": 6, "strongantimatthew": 6}
+# gini_min = {}
+# gini_max = {}
+# poly_app_deg = 2
+# x_vals = agen_nums
+# x_vals_dense = range(agen_nums.start,agen_nums.stop,agen_nums.step//2)
+# gini_data = np.loadtxt(script_path+"/data/gini_index_values-constant.dat")
+# 
+# #%% ploting
+# fig = mpl.figure.Figure(figsize=(12,10))
+# for i,curr_policy in enumerate(default_policies):
+#     for j,curr_eps in enumerate(default_eps_vals):
+#         
+#         axs = fig.add_subplot(4,4,1+i+4*j)
+#         plot_desc = str(curr_policy)+ ", " + r"$\epsilon$="+str(curr_eps)
+#         axs.grid(alpha=0.5,ls=':')
+#         
+#         axs.plot(x_vals_dense,gini_data[x_vals_dense],'k-.')
+#         
+#         for b in ["matthew", "strongmatthew", "antimatthew", "strongantimatthew" ]:
+#             gini_max[b] = [rd[(rd.default_policy==curr_policy) & (rd.default_boost == b) & (rd.default_eps == curr_eps) ][rd.num_agents==r]['Gini index'].max() for r in x_vals]
+#             gini_min[b] = [rd[(rd.default_policy==curr_policy) & (rd.default_boost == b) & (rd.default_eps == curr_eps) ][rd.num_agents==r]['Gini index'].min() for r in x_vals]
+#        
+#         
+#             # axs.plot(x_vals, gini_max[b], plot_marker[b], label=plot_label[b])
+#             # axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_max[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
+#             
+#             # axs.plot(x_vals, gini_min[b], plot_marker[b])
+#             # axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_min[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
+#             
+#             
+#             axs.plot(x_vals, gini_max[b], plot_marker[b], fillstyle='none', ms=plt_marker_size[b], label=plot_label[b])
+#             axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_max[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
+#             
+#             axs.plot(x_vals, gini_min[b], plot_marker[b],  fillstyle='none', ms=plt_marker_size[b] )
+#             axs.plot(x_vals, np.polyval(np.polyfit(x_vals,gini_min[b],poly_app_deg),x_vals), plot_marker[b][0]+":", linewidth=1)
+#             
+#        
+#             # axs.plot(gini_data)
+#             # axs.plot(x_vals_dense, gini_data[x_vals_dense],"k",linewidth=1, markersize=4)
+#             #axs.set_xlabel('Number of agents')
+#             axs.set_xlim((2,x_vals[-1]+15))
+#             axs.set_ylim((0.0,0.8))
+#             # axs.set_ylabel('Gini index')
+#             # axs.legend(ncol=1, columnspacing=0, labelspacing=0.5)
+#             # axs.set_title(plot_desc)
+#             
+#             axs.text(15, 0.60, plot_desc, rasterized=False, usetex=True)
+#             axs.set_xticks(x_vals)
+# 
+#             if i in [6,7,8]:
+#                 axs.set_xlabel('Number of agents')
+#             else:
+#                 axs.set_xticklabels([])           
+#                 
+#             axs.set_yticks(np.arange(0, 1, step=0.2))
+#                 
+#             if i in [0,4,8,12]:
+#                 axs.set_ylabel('Gini index')
+#             else:
+#                 axs.set_yticklabels([])
+# 
+# handles, labels = axs.get_legend_handles_labels()
+# fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5,-0.02), ncol=4)
+# 
+# display(fig)
+# 
+# fig.tight_layout()
+# fig.savefig("plots/"+ exp_desc +".pdf")
+# fig.savefig("plots/png/"+ exp_desc +".png")
 
 
 # #%% results form the batch execution
