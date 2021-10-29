65ea4dd11b956f53073142a5d33e8c6cc52e9272
Jarek Miszczak <jarek@miszczak.eu>
2021-10-29 15:12:27 +0200

    Code reformatting

--- models/sznajd1d/Sznajd1DModel.py
+++ models/sznajd1d/Sznajd1DModel.py
@@ -6,19 +6,22 @@ import mesa.space as ms
 import mesa.time as mt
 import mesa.datacollection as md
 
+
 def vote_result(model):
     opinions = [agent.opinion for agent in model.schedule.agents]
-    return sum(opinions)/model.num_agents
+    return sum(opinions) / model.num_agents
+
 
 def individual_votes(model):
     return [agent.opinion for agent in model.schedule.agents]
+
 
 class Sznajd1DAgent(mesa.Agent):
     # 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.opinion = self.random.choice([-1,1])
-    
+        self.opinion = self.random.choice([-1, 1])
+
     # update the opinion
     def step(self):
         self.update_opinion()
@@ -29,12 +32,10 @@ class Sznajd1DAgent(mesa.Agent):
         # get its opinion
         rn_op = self.model.grid.get_cell_list_contents([rn])[0].opinion
         # update opinions
-        if self.opinion*rn_op == 1:
+        if self.opinion * rn_op == 1:
             print("OK")
         else:
             print("Agrrrr")
-
-
 
 
 class Sznajd1DModel(mesa.Model):
@@ -46,9 +47,9 @@ class Sznajd1DModel(mesa.Model):
         self.x = [(x) for x in range(self.num_agents)]
 
         self.datacollector = md.DataCollector(
-                model_reporters = {'Average opinion' : vote_result, 'Agent opinions': individual_votes },
-                agent_reporters = {'Opinion': 'opinion'}
-                )
+            model_reporters={'Average opinion': vote_result, 'Agent opinions': individual_votes},
+            agent_reporters={'Opinion': 'opinion'}
+        )
 
         for uid in range(self.num_agents):
             a = Sznajd1DAgent(uid, self)
