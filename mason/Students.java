591a031fc3c4da6f3d3c82b58d477c4a1e15aecc
Jarek Miszczak <jarek@miszczak.eu>
2022-12-04 22:13:59 +0100

    Testing MASON

---
+++ mason/Students.java
@@ -0,0 +1,16 @@
+/*
+ * Code based on the MASON tutorial
+ * 
+ */
+import sim.engine.*;
+
+public class Students extends SimState {
+  public Students(long seed){
+    super(seed);
+  }
+
+  public static void main(String[] args){
+    System.out.println("[INFO]");
+    System.exit(0);
+  }
+}
