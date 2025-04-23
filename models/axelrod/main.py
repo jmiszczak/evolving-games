b844013ac253dbd26351fc9ba6f4ea649b767d70
Jarek Miszczak <jarek@miszczak.eu>
2025-04-23 19:01:58 +0200

    main file

---
+++ models/axelrod/main.py
@@ -0,0 +1,10 @@
+
+from AxelrodModel import AxelrodModel
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    model = AxelrodModel(10)
+    model.step()
+
+    # model.print_agents()
